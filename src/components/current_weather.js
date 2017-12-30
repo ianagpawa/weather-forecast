4db036e831ec65a23cbfa5cadeac7e73171270a3
Ian Agpawa <agpawaji@gmail.com>
2017-12-29 21:33:56 -0500

    feat: Start add current weather component

--- src/components/current_weather.js
+++ src/components/current_weather.js
@@ -5,17 +5,20 @@ export default (props) => {
         <div>
             <div className='left col-xs-4'>
                 <div className='icon'>
-
+                    Icon
                 </div>
                 <div className='condition'>
+                    Condition
                 </div>
             </div>
             <div className='right col-xs-8'>
                 <div className='temp'>
+                    {props.temp} temp
                 </div>
                 <div className='lower-right'>
-                    <p>Low-High</p>
-                    <p>Humidity</p>
+                    <p>{props.low} LOW</p>
+                    <p>{props.humidity} Humidity</p>
+                    <p>{props.pressure} Pressure</p>
                 </div>
             </div>
         </div>
--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Chart from "../components/chart";
+import Current from "../components/current_weather"
 
 
 class WeatherList extends Component {
     renderWeather(cityData) {
         const city_name = cityData.city.name;
         const temps = cityData.list.map(weather => weather.main.temp);
+        const tempMin = cityData.list.map(weather => weather.main.temp_min)[0]
         const pressures = cityData.list.map(weather => weather.main.pressure);
         const humidities = cityData.list.map(weather => weather.main.humidity);
+
+        const currentTemp = temps[0]
+        const currentPres = pressures[0]
+        const currentHum = humidities[0]
 
         return (
             <tr key={city_name}>
                 <td>{city_name}</td>
+                <td><Current low={tempMin} temp={currentTemp} humidity={currentHum} pressure={currentPres}/></td>
                 <td><Chart data={temps} color='red' units='K'/></td>
                 <td><Chart data={pressures} color='green' units='hPa'/></td>
                 <td><Chart data={humidities} color='black' units='%'/></td>
