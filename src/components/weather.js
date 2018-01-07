3bcf2205afd282b7d7032d52ef9f4565ede5a6ef
Ian Agpawa <agpawaji@gmail.com>
2018-01-06 20:34:01 -0500

    refactor: Update prop of DailyWeather for weekday

--- src/components/weather.js
+++ src/components/weather.js
@@ -5,7 +5,7 @@ import { fahrenheit } from '../components/fahrenheit';
 export default (props) => {
     return (
             <li className="weather-group-item">
-                <h3>{props.dayD}</h3>
+                <h3>{props.weekday}</h3>
                 <h2>{fahrenheit(props.temp)}&#8457;</h2>
                 <i className={props.icon}></i>
                 <p>{props.condition}</p>
