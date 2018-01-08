58ec69127da47027a727dd56c8dbace4fbf9daba
Ian Agpawa <agpawaji@gmail.com>
2018-01-08 13:44:17 -0500

    refactor: Update temp for forecast component to h4

--- src/components/weather.js
+++ src/components/weather.js
@@ -6,7 +6,7 @@ export default (props) => {
     return (
             <div className="daily-weather col-sm-2">
                 <h3>{props.weekday}</h3>
-                <h2>{fahrenheit(props.temp)}&#8457;</h2>
+                <h4>{fahrenheit(props.temp)}&#8457;</h4>
                 <i className={props.icon}></i>
                 <p>{props.condition}</p>
             </div>
