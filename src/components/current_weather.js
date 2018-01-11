c5ea8cd9962396cbe35490d0eafb085972d7d479
Ian Agpawa <agpawaji@gmail.com>
2018-01-11 17:00:57 -0500

    refactor: Update integrate weather component into current weather

--- src/components/current_weather.js
+++ src/components/current_weather.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import 'weather-icons/css/weather-icons.css';
 import { fahrenheit } from './functions';
+import DailyWeather from './weather';
+
 
 export default (props) => {
+    const size = "left col-xs-4 text-center";
     return (
         <div className='col-xs-6 col-xs-offset-3'>
             <div className='col-xs-8 col-xs-offset-2'><h1>{props.name}</h1></div>
-            <div className='left col-xs-4 text-center'>
-                <h3>{props.day}</h3>
-                <h2>{fahrenheit(props.temp)}&#8457;</h2>
-                <i className={props.icon}></i>
-                <p>{props.condition}</p>
-            </div>
+            <DailyWeather
+                size={size}
+                weekday={props.day}
+                temp={props.temp}
+                condition={props.condition}
+                icon={props.icon}
+            />
             <div className='right col-xs-7 col-xs-offset-1'>
                     <p>{props.description}</p>
                     <p>{fahrenheit(props.low)}&#8457; - {fahrenheit(props.high)}&#8457;</p>
