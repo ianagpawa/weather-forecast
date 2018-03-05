95c7590ba91503e4ab20372204a80eab6b43f8fe
Ian Agpawa <agpawaji@gmail.com>
2018-03-04 23:04:01 -0500

    refactor: Start error handling for city name

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,13 +4,39 @@ import API_KEY from './client_secrets'
 const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}` ;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
+export const HANDLE_AXIOS_ERROR = 'HANDLE_AXIOS_ERROR';
+
+function fetchWeatherSuccess(response){
+    return {
+        type: FETCH_WEATHER,
+        payload: response
+    };
+}
+
+function fetchWeatherError(error){
+    const eUrl = `${ROOT_URL}&q=Tampa,us`;
+    const eRequest = axios.get(eUrl);
+    return {
+        type: HANDLE_AXIOS_ERROR,
+        payload: eRequest,
+        error: true
+    };
+}
 
 export function fetchWeather(city) {
     const url = `${ROOT_URL}&q=${city},us`;
     const request = axios.get(url);
 
-    return {
-        type: FETCH_WEATHER,
-        payload: request
-    };
+    return request
+        .then((response) => {
+            return fetchWeatherSuccess(response);
+        })
+        .catch((error) => {
+            return fetchWeatherError(error);
+        })
+
+    // return {
+    //     type: FETCH_WEATHER,
+    //     payload: request
+    // };
 }
--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,9 +1,14 @@
-import { FETCH_WEATHER } from "../actions/index";
+import { FETCH_WEATHER, HANDLE_AXIOS_ERROR } from "../actions/index";
 
 export default function(state=[], action){
+    if (action.error){
+        action.type = HANDLE_AXIOS_ERROR;
+    }
     switch (action.type) {
     case FETCH_WEATHER:
         // return state.concat([action.payload.data]); same as below
+        return [ action.payload.data ];
+    case HANDLE_AXIOS_ERROR:
         return [ action.payload.data ];
     default:
         // Do nothing
